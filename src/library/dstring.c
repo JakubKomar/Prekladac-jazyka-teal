528a01a9916ae6a77a665465d2273cb4f405bbfc
komca12 <57370991+komca12@users.noreply.github.com>
2021-07-28 15:32:42 +0200

    libary added

---
+++ src/library/dstring.c
@@ -0,0 +1,72 @@
+/**
+ * Dynamic string 
+ * @brief   Dynamic string libary
+ *
+ * @authors Jakub Komárek (xkomar33)
+ * @date    28-07-2021
+ * @version 1.0
+ */
+#include "dstring.h"
+
+#define STR_LEN_INC 8   //base size memory alocation of string
+
+int strInit(string *s)
+{
+   if ((s->str = (char*) malloc(STR_LEN_INC)) == NULL)
+      return 1;
+   s->str[0] = '\0';
+   s->length = 0;
+   s->allocSize = STR_LEN_INC;
+   return 0;
+}
+
+void strFree(string *s)
+{
+   free(s->str);
+}
+
+void strClear(string *s)
+{
+   s->str[0] = '\0';
+   s->length = 0;
+}
+
+int strAddChar(string *s1, char c)
+{
+   if (s1->length + 1 >= s1->allocSize)
+   {
+      if ((s1->str = (char*) realloc(s1->str, s1->length + STR_LEN_INC)) == NULL)
+         return 1;
+      s1->allocSize = s1->length + STR_LEN_INC;
+   }
+   s1->str[s1->length] = c;
+   s1->length++;
+   s1->str[s1->length] = '\0';
+   return 0;
+}
+
+int strAddString(string *s1, char *s2)
+{
+   int i=0;
+   while(s2[i]!='\0')
+   {
+      if(strAddChar(s1,s2[i]))
+         return 1;
+      i++;
+   }
+   return 0;
+}
+
+int strCopyString(string *s1, string *s2)
+{
+   int newLength = s2->length;
+   if (newLength >= s1->allocSize)
+   {
+      if ((s1->str = (char*) realloc(s1->str, newLength + 1)) == NULL)
+         return 1;
+      s1->allocSize = newLength + 1;
+   }
+   strcpy(s1->str, s2->str);
+   s1->length = newLength;
+   return 0;
+}
---
+++ src/library/dstring.h
@@ -0,0 +1,55 @@
+/**
+ * Dynamic string 
+ * @brief   Dynamic string libary
+ *
+ * @authors Jakub Komárek (xkomar33)
+ * @date    28-07-2021
+ * @version 1.0
+ */
+#include <string.h>
+#include <malloc.h>
+typedef struct
+{
+  char* str;		// misto pro dany retezec ukonceny znakem '\0'
+  int length;		// skutecna delka retezce
+  int allocSize;	// velikost alokovane pameti
+} string;
+
+/**
+ * string creation
+ * @return 0-success,1-error
+ */
+int strInit(string *s);
+
+/**
+ * string destruction
+ */
+void strFree(string *s);
+
+/**
+ * hardcopy of string
+ * @param string to clearing
+ * @return 0-success,1-error
+ */ 
+int strCopyString(string *s1, string *s2);
+
+/**
+ * clear string
+ * @param string to clearing
+ */ 
+void strClear(string *s);
+
+/**
+ * add one charakter to end of string
+ * @param *s1-string for modification
+ * @param c-charakter to add
+ */ 
+int strAddChar(string *s1, char c);
+
+/**
+ * add string to end of string
+ * @param *s1-string for modification
+ * @param *s2-string which will be added to end
+ */ 
+int strAddString(string *s1, char *s2);
+
