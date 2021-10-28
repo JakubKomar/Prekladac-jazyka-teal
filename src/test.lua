2f769eefd99def4a7bac2ab14bcf54c30f2318a4
Krivoss <jakub.kriv@gmail.com>
2021-10-28 15:31:53 +0200

    fixed ord and substr

--- src/test.lua
+++ src/test.lua
@@ -1,16 +1,7 @@
 -- ret params function test 0
 require "ifj21"
 
-function func(a:integer,b:string,c:number):integer,string,string
-
-    return a,"ahoj","ahoj"
-end
-
-local a:integer=95,85,45+55,"ahoj"
-
-local b:string
-
-local c:string
-
-a,b=func(2,"zmrd",2)
-write(a,b)
+local s:string="hello world"
+local sub:string=substr(s, 5, 9)
+local o:integer=ord(s,4)
+write(sub, "\n", o, "\n")
